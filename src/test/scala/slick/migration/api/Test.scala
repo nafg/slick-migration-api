2b34b09831fd2df713e85ca816fea678cc8ca3ec
Naftoli Gugenheim <naftoligug@gmail.com>
2013-06-24 18:46:21 -0400

    Test {Create,Drop}ForeignKey

--- src/test/scala/slick/migration/api/Test.scala
+++ src/test/scala/slick/migration/api/Test.scala
@@ -9,6 +9,7 @@ import org.scalatest.matchers.ShouldMatchers
 import com.typesafe.slick.testkit.util._
 
 import scala.slick.driver._
+import scala.slick.lifted.ForeignKeyAction
 
 import jdbc.meta._
 
@@ -55,6 +56,7 @@ class Test extends fixture.FunSuite with ShouldMatchers with Inside with DbFixtu
     val rms1: ReversibleMigrationSeq = rms // verify the type
     rms should equal (new ReversibleMigrationSeq(rm, rm, rm))
   }
+
   test("CreateTable, DropTable") { implicit fix =>
     import fix._
     import driver.simple._
@@ -88,5 +90,60 @@ class Test extends fixture.FunSuite with ShouldMatchers with Inside with DbFixtu
     createTable.reverse()
 
     tables.list should equal (before)
+  }
+
+  test("CreateForeignKey, DropForeignKey") { implicit fix =>
+    import fix._
+    import driver.simple._
+
+    object table1 extends Table[(Long, Long)]("table1") {
+      def id = column[Long]("id", O.PrimaryKey)
+      def other = column[Long]("other")
+
+      def * = id ~ other
+      // lazy val so equality works
+      lazy val fk = foreignKey("fk_other", other, table2)(_.id, ForeignKeyAction.Cascade, ForeignKeyAction.Cascade)
+    }
+    object table2 extends Table[Long]("table2") {
+      def id = column[Long]("id", O.PrimaryKey)
+      def * = id
+    }
+
+    CreateTable(table1)(_.id, _.other)()
+    CreateTable(table2)(_.id)()
+
+    def fks = MTable.getTables.to[Set] map { t =>
+      (
+        t.name.name,
+        t.getImportedKeys.list map { fk =>
+          (fk.pkTable.name, fk.pkColumn, fk.fkTable.name, fk.fkColumn, fk.updateRule, fk.deleteRule, fk.fkName)
+        }
+      )
+    }
+
+    val before = fks
+
+    before should equal (Set(
+      ("table1", Nil),
+      ("table2", Nil)
+    ))
+
+    val createForeignKey = CreateForeignKey(table1.fk)
+    createForeignKey()
+
+    fks should equal (Set(
+      ("table1", ("table2", "id", "table1", "other", ForeignKeyAction.Cascade, ForeignKeyAction.Cascade, Some("fk_other")) :: Nil),
+      ("table2", Nil)
+    ))
+
+    val dropForeignKey = createForeignKey.reverse
+    inside(dropForeignKey.migrations.toList) {
+      case DropForeignKey(fk) :: Nil =>
+        Seq(fk) should equal (table1.fk.fks)
+    }
+
+    dropForeignKey()
+
+    fks should equal (before)
   }
 }
