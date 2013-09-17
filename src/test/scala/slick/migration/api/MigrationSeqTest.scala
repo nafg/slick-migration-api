c377f7e45c8dabb0eae6fe0329a921fae013bddd
Naftoli Gugenheim <naftoligug@gmail.com>
2013-09-17 11:52:14 -0400

    tests: a bit of stylistic improvement

--- src/test/scala/slick/migration/api/MigrationSeqTest.scala
+++ src/test/scala/slick/migration/api/MigrationSeqTest.scala
@@ -10,13 +10,13 @@ class MigrationSeqTest extends FunSuite with ShouldMatchers {
     object migrations extends Migrations(H2Driver)
     import migrations._
     val m = new Migration {
-      def apply()(implicit s: driver.simple.Session) = ()
+      def apply()(implicit s: H2Driver.simple.Session) = ()
     }
     m & m & m should equal (MigrationSeq(m, m, m))
 
     val rm = new ReversibleMigration {
-      def apply()(implicit s: driver.simple.Session) = ()
-      def reverse = ???
+      def apply()(implicit s: H2Driver.simple.Session) = ()
+      def reverse = this
     }
 
     val rms = rm & rm & rm
